3fc47e2d56fa6d67e72c2e756566524cc02ee698
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2024-05-06 21:40:03 +0300

    add firebase messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -5,6 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseMessaging.instance.requestPermission();
   runApp(const MainApp());
 }
 
@@ -16,9 +18,44 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Token(),
+          ),
         ),
       ),
     );
+  }
+}
+
+class Token extends StatefulWidget {
+  const Token({super.key});
+
+  @override
+  State<Token> createState() => _TokenState();
+}
+
+class _TokenState extends State<Token> {
+  String token = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.instance.getToken().then((value) {
+      token = value ?? 'Unable to get token';
+      setState(() {});
+      print(token);
+    });
+
+    FirebaseMessaging.instance.onTokenRefresh.listen((receivedToken) {
+      token = receivedToken;
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(token);
   }
 }
